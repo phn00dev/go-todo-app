afc4156863e27c6a8f4a17003ee92c3db0527964
phn00dev <hudayberdipolat@gmail.com>
2024-11-20 11:10:29 +0500

    Authentication. JWT tokens.

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -9,12 +9,12 @@ import (
 )
 
 func (h *Handler) signUp(c *gin.Context) {
-	var inputs todo.User
-	if err := c.BindJSON(&inputs); err != nil {
+	var regesterRequest todo.User
+	if err := c.BindJSON(&regesterRequest); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	id, err := h.services.Authorization.CreateUser(inputs)
+	id, err := h.services.Authorization.CreateUser(regesterRequest)
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
@@ -25,6 +25,25 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) signIn(c *gin.Context) {
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) signIn(c *gin.Context) {
+	var loginRequest signInInput
+	if err := c.BindJSON(&loginRequest); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// call login service
+	token, err := h.services.Authorization.GenerateToken(loginRequest.Username, loginRequest.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]any{
+		"bearer_token": token,
+	})
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,24 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/phn00dev/go-todo-app"
 	"github.com/phn00dev/go-todo-app/pkg/repository"
 )
 
-const solt = "asdad&^2312556t&hbdfs4)sdagd*$Y"
+const (
+	solt       = "asdad&^2312556t&hbdfs4)sdagd*$Y"
+	tokenTTL   = 12 * time.Hour
+	signingKey = "15dasdasf&^hddasdw5435fdsf"
+)
+
+type tokenClaims struct {
+	UserId int `json:"user_id"`
+	jwt.StandardClaims
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +33,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	// get user data
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenClaims{
+		user.Id,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
